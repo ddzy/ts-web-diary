d406c42eafcc6fabda87a361a864e0018cc0ae1c
yang <1766083035@qq.com>
2018-11-07 13:00:05 +0800

    style * 剔除无用注释

--- src/constants/constants.tsx
+++ src/constants/constants.tsx
@@ -1,24 +1,9 @@
 
 
 /**
- * 标签 
+ * 文章所属具体内容标签
  */
 export const TAG_COLOR_PICKER: object = {
-  // '爱情': 'magenta',
-  // '旅途': 'red',
-  // '职场': 'volcano',
-  // '围城': 'orange',
-  // '校园': 'gold',
-  // '宝宝': 'lime',
-  // '寂寞': 'green',
-  // '朋友': 'cyan',
-  // '思念': 'blue',
-  // '烦恼': 'geekblue',
-  // '压力': 'purple',
-  // '孤独': 'pink',
-  // '琐事': '#d50',
-  // '心情': '#09c',
-
   'react': 'magenta',
   'vue': 'red',
   'angular': 'volcano',
