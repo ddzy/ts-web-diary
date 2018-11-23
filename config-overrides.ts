2441cafc395b4e5fd9bb5ff004e7dc2dad36bb6c
yang <1766083035@qq.com>
2018-11-23 10:31:14 +0800

    chore: 修改文件后缀

--- config-overrides.ts
+++ config-overrides.ts
@@ -1,10 +1,10 @@
 const tsImportPluginFactory = require('ts-import-plugin')
 const { getLoader } = require("react-app-rewired");
 
-module.exports = function override(config, env) {
+module.exports = function override(config: any) {
   const tsLoader = getLoader(
     config.module.rules,
-    rule =>
+    (rule: any) =>
       rule.loader &&
       typeof rule.loader === 'string' &&
       rule.loader.includes('ts-loader')
