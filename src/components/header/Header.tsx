52a51e0ec4cb65857a3e959904c7147a44bdced1
yang <1766083035@qq.com>
2018-12-16 11:08:54 +0800

    :art: 优化code结构

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,177 +1,38 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
-import { Link, withRouter, RouteComponentProps } from 'react-router-dom';
 import {
-  Button,
-  Modal,
   Affix,
 } from 'antd';
 
-import headerNavConfig from '../../config/headerNav.config';
 import {
   HeaderWrapper,
-  MainNavItem,
-  PopoverContent,
-  PopoverContentList,
-  PopoverListItem,
-  PopoverItemText,
 } from './style';
 import HeaderMain from './header_main/HeaderMain';
 
 
-export interface IHeaderProps extends RouteComponentProps<any> {
+export interface IHeaderProps {
   AuthRouteReducer: {
     isAuth: boolean;
     username: string;
     useravatar: string;
   };
 };
-interface IHeaderState {
-  navList: object[];
-};
 
 
-class Header extends React.Component<IHeaderProps, IHeaderState> {
-
-  public readonly state = {
-    navList: [
-      { path: '/home', value: '首页', children: null },
-      { path: '/article', value: '文章', children: null },
-    ],
-  }
-
-  public componentDidMount(): void {
-    this.handleDealNavList();
-  }
-
-  public handleDealNavList = (): void => {
-    const { pathname } = this.props.location;
-
-    const result = headerNavConfig.filter((item: any) => {
-      return item.path === pathname;
-    });
-
-    /\/details\/\w*/.test(pathname)
-      && result.push({
-        path: '/details', value: '详情', children: null
-      });
-
-    /\/collection\/\w*/.test(pathname)
-      && result.push({
-        path: '/collection',
-        value: '收藏',
-        children: null,
-      });
-
-    this.setState((prevState) => {
-      return {
-        navList: prevState.navList.concat(result),
-      };
-    }, () => {
-      // 默认选中菜单栏
-      const oUl = document.getElementById('header-navbar');
-      const oLi = oUl && oUl.querySelectorAll('li');
-
-      oLi && Array.from(oLi).forEach((item) => {
-        // item.getAttribute('data-pane') === pathname
-        pathname.includes(item.getAttribute('data-pane') || '')
-          && item.classList.add('header-active');
-      });
-    });
-  }
-
-  /**
-   * 初始化菜单栏
-   */
-  public initHeaderNavList = (): JSX.Element[] => {
-    return this.state.navList.map((item) => {
-      return (
-        <MainNavItem
-          key={item.path}
-          data-pane={item.path}
-        >
-          <Link to={item.path}>{item.value}</Link>
-        </MainNavItem>
-      );
-    });
-  }
-
-  /**
-   * 退出登录
-   */
-  public handleLogout: React.MouseEventHandler = (): void => {
-    Modal.confirm({
-      content: '确定要退出登录吗?',
-      onOk: () => {
-        localStorage.removeItem('token');
-        localStorage.removeItem('userid');
-        this.props.history.push('/login');
-      },
-    });
-  }
-
-  /**
-   * 个人中心
-   */
-  public handleToMe: React.MouseEventHandler = (): void => {
-    this.props.history.push('/me');
-  }
-
-  /**
-   * 写文章
-   */
-  public handleToWrite: React.MouseEventHandler = () => {
-    this.props.history.push('/publish');
-  }
-
-  public initPopoverContent = (): JSX.Element => {
-    return (
-      <PopoverContent>
-        <PopoverContentList>
-          <PopoverListItem>
-            <Button
-              htmlType="button"
-              type="primary"
-              icon="setting"
-              block={true}
-              onClick={this.handleToMe}
-            >
-              <PopoverItemText>
-                个人中心
-              </PopoverItemText>
-            </Button>
-          </PopoverListItem>
-          <PopoverListItem>
-            <Button
-              htmlType="button"
-              type="danger"
-              icon="logout"
-              block={true}
-              onClick={this.handleLogout}
-            >
-              <PopoverItemText>
-                退出登录
-              </PopoverItemText>
-            </Button>
-          </PopoverListItem>
-        </PopoverContentList>
-      </PopoverContent>
-    );
-  }
-
-  public render(): JSX.Element {
-    return (
-      <Affix
-        offsetTop={1}
-      >
-        <HeaderWrapper>
-          <HeaderMain
-            authInfo={{...this.props.AuthRouteReducer}}
-          />
-        </HeaderWrapper>
-      </Affix>
-    );
-  }
+const Header: React.SFC<IHeaderProps> = (
+  props: IHeaderProps,
+): JSX.Element => {
+  return (
+    <Affix
+      offsetTop={1}
+    >
+      <HeaderWrapper>
+        <HeaderMain
+          authInfo={{...props.AuthRouteReducer}}
+        />
+      </HeaderWrapper>
+    </Affix>
+  );
 }
 
 
@@ -181,6 +42,6 @@ function mapStateToProps(state: any) {
   };
 }
 
-export default withRouter(connect(
+export default connect(
   mapStateToProps,
-)(Header)) as React.ComponentClass<any>;
+)(Header) as React.ComponentClass<any>;
