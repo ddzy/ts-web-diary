aae07941ed08723ffaf45811b7d580db58498c3a
yang <1766083035@qq.com>
2018-12-17 20:36:47 +0800

    :sparkles: 新增header_search_热搜组件

--- src/components/header/header_main/header_main_search/HeaderMainSearch.tsx
+++ src/components/header/header_main/header_main_search/HeaderMainSearch.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import {
+  Popover,
   Input,
 } from 'antd';
 
@@ -7,6 +8,9 @@ import {
   SearchWrapper,
   SearchMain,
   SearchMainInput,
+  PopContentBox,
+  PopContentList,
+  PopContentListItem,
 } from './style';
 
 
@@ -17,15 +21,47 @@ const HeaderMainSearch: React.SFC<IHeaderMainSearchProps> = (
   props: IHeaderMainSearchProps,
 ): JSX.Element => {
 
+  /**
+   * 处理初始化热搜popover-content
+   */
+  function handleInitPopContent(): JSX.Element[] {
+    const hotSearchArr: string[] = [
+      'Eruda 一个被人遗忘的调试神器',
+      '结合 Google quicklink，react 项目实现页面秒开',
+      'React 实战：设计模式和最佳实践',
+      '你（可能）不知道的web api',
+      '关于依赖注入（typescript）',
+    ];
+
+    return hotSearchArr.map((hot, i) => (
+      <PopContentListItem
+        key={i}
+      >{hot}🎉</PopContentListItem>
+    ));
+  }
+
   return (
     <SearchWrapper>
       <SearchMain>
         <SearchMainInput>
-          <Input.Search
-            size="large"
-            placeholder={`搜索您想要的内容...`}
-            enterButton
-          />
+          <Popover
+            title={`文章热搜榜`}
+            placement="bottom"
+            trigger="focus"
+            content={
+              <PopContentBox>
+                <PopContentList>
+                  {handleInitPopContent()}
+                </PopContentList>
+              </PopContentBox>
+            }
+          >
+            <Input.Search
+              size="large"
+              placeholder={`搜索您想要的文章...`}
+              enterButton
+            />
+          </Popover>
         </SearchMainInput>
       </SearchMain>
     </SearchWrapper>
--- src/components/header/header_main/header_main_search/style.tsx
+++ src/components/header/header_main/header_main_search/style.tsx
@@ -8,3 +8,18 @@ export const SearchMain = styled.div`
   /* padding: 0 9rem; */
 `;
 export const SearchMainInput = styled.div``;
+
+export const PopContentBox = styled.div`
+  min-width: 18.75rem;
+`;
+export const PopContentList = styled.ul``;
+export const PopContentListItem = styled.li`
+  padding: 0 .5rem;
+  color: initial;
+  line-height: 2.5rem;
+  cursor: pointer;
+  &:hover {
+    background-color: #f6f6f6;
+    color: #1890ff;
+  }
+`;
