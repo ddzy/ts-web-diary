24186746f82c4bbee468b4ece5179bfdc00ec1b7
yang <1766083035@qq.com>
2018-12-27 16:41:17 +0800

    :hooks重构DetailsControl

--- src/pages/details/details_control/DetailsControl.tsx
+++ src/pages/details/details_control/DetailsControl.tsx
@@ -37,127 +37,120 @@ export interface IDetailsControlProps {
     collectionId: string,
   ) => void;
 };
-interface IDetailsControlState { };
 
 
 /**
  * 左侧固钉控制栏
  */
-class DetailsControl extends React.PureComponent<
-  IDetailsControlProps,
-  IDetailsControlState
-  > {
-
-  public readonly state = {}
+const DetailsControl = React.memo<IDetailsControlProps>((
+  props: IDetailsControlProps,
+): JSX.Element => {
 
   /**
    * 解决二次渲染问题
    */
-  public handleAnchorClick: React.MouseEventHandler<HTMLElement> = (
+  function handleAnchorClick(
     e: React.MouseEvent,
-  ): void => {
+  ): void {
     e.preventDefault();
   }
 
-  public render(): JSX.Element {
-    return (
-      <React.Fragment>
-        <FixedControlContainer>
-          <FixedControlContent>
-            <FixedControlList>
-              {/* 点赞 */}
-              <Tooltip title="赞一个" placement="right">
-                <FixedControlListItem>
-                  <Icon
-                    className={`
-                    fixed-control-bar-star
-                    ${
-                      this.props.isLiked
-                      && 'fixed-control-bar-star-active'
-                      }
-                  `}
-                    type="star"
-                    theme="filled"
-                    onClick={this.props.onControlBarStar}
-                  />
-                </FixedControlListItem>
-              </Tooltip>
-              {/* 评论 */}
-              <Tooltip title="去评论" placement="right">
-                <FixedControlListItem>
-                  <Anchor onClick={this.handleAnchorClick}>
-                    <Anchor.Link
-                      title={
-                        <Icon
-                          className="fixed-control-bar-message"
-                          type="message"
-                          theme="filled"
-                        />
-                      }
-                      href="#left-comment-container"
-                    />
-                  </Anchor>
-                </FixedControlListItem>
-              </Tooltip>
-              {/* 收藏 */}
-              <Tooltip title="收藏" placement="right">
-                <FixedControlListItem>
-                  <Popover
-                    trigger="click"
-                    placement="right"
-                    title="我的收藏夹"
-                    content={
-                      <DetailsControlCollections
-                        collections={this.props.collections}
-
-                        collectionInputValue={this.props.collectionInputValue}
-                        onCollectionsInputChange={this.props.onCollectionsInputChange}
-                        onSendCollection={this.props.onSendCollection}
-
-                        onSaveToCollection={this.props.onSaveToCollection}
+  return (
+    <React.Fragment>
+      <FixedControlContainer>
+        <FixedControlContent>
+          <FixedControlList>
+            {/* 点赞 */}
+            <Tooltip title="赞一个" placement="right">
+              <FixedControlListItem>
+                <Icon
+                  className={`
+                  fixed-control-bar-star
+                  ${
+                    props.isLiked
+                    && 'fixed-control-bar-star-active'
+                    }
+                `}
+                  type="star"
+                  theme="filled"
+                  onClick={props.onControlBarStar}
+                />
+              </FixedControlListItem>
+            </Tooltip>
+            {/* 评论 */}
+            <Tooltip title="去评论" placement="right">
+              <FixedControlListItem>
+                <Anchor onClick={handleAnchorClick}>
+                  <Anchor.Link
+                    title={
+                      <Icon
+                        className="fixed-control-bar-message"
+                        type="message"
+                        theme="filled"
                       />
                     }
-                  >
-                    <Icon
-                      className="fixed-control-bar-collection"
-                      type="heart"
-                      theme="filled"
+                    href="#left-comment-container"
+                  />
+                </Anchor>
+              </FixedControlListItem>
+            </Tooltip>
+            {/* 收藏 */}
+            <Tooltip title="收藏" placement="right">
+              <FixedControlListItem>
+                <Popover
+                  trigger="click"
+                  placement="right"
+                  title="我的收藏夹"
+                  content={
+                    <DetailsControlCollections
+                      collections={props.collections}
+
+                      collectionInputValue={props.collectionInputValue}
+                      onCollectionsInputChange={props.onCollectionsInputChange}
+                      onSendCollection={props.onSendCollection}
+
+                      onSaveToCollection={props.onSaveToCollection}
                     />
-                  </Popover>
-                </FixedControlListItem>
-              </Tooltip>
-              <FixedControlListItem>
-                分享
+                  }
+                >
+                  <Icon
+                    className="fixed-control-bar-collection"
+                    type="heart"
+                    theme="filled"
+                  />
+                </Popover>
+              </FixedControlListItem>
+            </Tooltip>
+            <FixedControlListItem>
+              分享
+          </FixedControlListItem>
+            <FixedControlListItem>
+              <Icon
+                type="qq"
+                theme="outlined"
+              />
             </FixedControlListItem>
-              <FixedControlListItem>
-                <Icon
-                  type="qq"
-                  theme="outlined"
-                />
-              </FixedControlListItem>
-              <FixedControlListItem>
-                <Icon
-                  type="twitter"
-                  theme="outlined"
-                />
-              </FixedControlListItem>
-              <FixedControlListItem>
-                <Icon
-                  type="weibo-circle"
-                  theme="outlined"
-                />
-              </FixedControlListItem>
-            </FixedControlList>
-          </FixedControlContent>
-        </FixedControlContainer>
+            <FixedControlListItem>
+              <Icon
+                type="twitter"
+                theme="outlined"
+              />
+            </FixedControlListItem>
+            <FixedControlListItem>
+              <Icon
+                type="weibo-circle"
+                theme="outlined"
+              />
+            </FixedControlListItem>
+          </FixedControlList>
+        </FixedControlContent>
+      </FixedControlContainer>
 
-        {/* Global Style Set */}
-        <GlobalStyleSet />
-      </React.Fragment>
-    );
-  }
-
-}
+      {/* Global Style Set */}
+      <GlobalStyleSet />
+    </React.Fragment>
+  );
+})
 
 
 export default DetailsControl;
