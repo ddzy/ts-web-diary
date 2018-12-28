e08647aecf8b0339ed067ec0406e54232c67c882
yang <1766083035@qq.com>
2018-12-28 16:38:14 +0800

    :racehorse: Admin解决路由多次点击渲染多次的问题

--- src/Admin.tsx
+++ src/Admin.tsx
@@ -1,50 +1,74 @@
 import * as React from 'react';
+import { Location } from 'history';
 
 import Header from './components/header/Header';
 
 
 export interface IAdminProps {
-  location: any;
+  location: Location;
   children: any;
 };
+interface IAdminState {
+  pathname: string;
+}
 
-const Admin = React.memo<IAdminProps>((
-  props: IAdminProps,
-) => {
-  React.useEffect(() => {
-    function aidedHandleMouseWheel(
-      e: WheelEvent,
-    ): void {
-      const nDeltaY = e.deltaY as number;
-      const oHeaderContainer = document
-        .querySelector('#header-main-container') as HTMLDivElement;
-
-      // ** 处理header滚动状态 **
-      oHeaderContainer.style.cssText += `
-        transform: translateY(${
-          nDeltaY > 0 ? '-100%' : 0
-        });
-      `;
-    }
-
-    window.addEventListener('wheel', aidedHandleMouseWheel);
-
-    return () => {
-      window.removeEventListener('wheel', aidedHandleMouseWheel);
+class Admin extends React.Component<IAdminProps, IAdminState> {
+  public static getDerivedStateFromProps(
+    nextProps: IAdminProps,
+  ): IAdminState {
+    return {
+      pathname: nextProps.location.pathname,
     };
-  }, []);
+  }
 
-  return (
-    <div className="admin-wrapper">
-      <div
-        className="admin-content"
-      >
-        <Header location={props.location} />
-        {props.children}
+  public readonly state: IAdminState = {
+    pathname: '',
+  }
+
+  public componentDidMount(): void {
+    window.addEventListener('wheel', this.aidedHandleMouseWheel);
+  }
+
+  public componentWillUnmount(): void {
+    window.removeEventListener('wheel', this.aidedHandleMouseWheel);
+  }
+
+  public shouldComponentUpdate(
+    nextProps: IAdminProps,
+  ): boolean {
+    const currentPathname = this.state.pathname;
+    const nextPathname = nextProps.location.pathname;
+
+    return nextPathname !== currentPathname;
+  }
+
+  public aidedHandleMouseWheel(
+    e: WheelEvent,
+  ): void {
+    const nDeltaY = e.deltaY as number;
+    const oHeaderContainer = document
+      .querySelector('#header-main-container') as HTMLDivElement;
+
+    // ** 处理header滚动状态 **
+    oHeaderContainer.style.cssText += `
+      transform: translateY(${
+        nDeltaY > 0 ? '-100%' : 0
+      });
+    `;
+  }
+
+  public render(): JSX.Element {
+    return (
+      <div className="admin-wrapper">
+        <div
+          className="admin-content"
+        >
+          <Header location={this.props.location} />
+          {this.props.children}
+        </div>
       </div>
-    </div>
-  );
-});
-
+    );
+  }
+}
 
 export default Admin;
