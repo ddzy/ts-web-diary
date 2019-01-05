6892437be042f4008c9d40334abf1961af9e713c
yang <1766083035@qq.com>
2019-01-05 17:02:23 +0800

    :hammer: 重构提取BaseCommentItemAvatar组件

--- src/components/widget/base_comment_item/base_comment_item_title/BaseCommentItemTitle.tsx
+++ src/components/widget/base_comment_item/base_comment_item_title/BaseCommentItemTitle.tsx
@@ -1,192 +1,23 @@
 import * as React from 'react';
-import {
-  Popover,
-  Avatar,
-  Divider,
-  Row,
-  Col,
-  Button,
-  Spin,
-} from 'antd';
 
 import {
   TitleContainer,
-  PopoverTitleContainer,
-  PopoverContentContainer,
-  PopoverContentMain,
-  PopoverTitleMain,
-  TitleMainAvatar,
-  TitleMainName,
-  ContentMainArticleCountBox,
-  ContentMainArticleCountText,
-  ContentMainArticleCountTip,
-  ContentMainFocusedCountBox,
-  ContentMainFocusedCountText,
-  ContentMainFocusedCountTip,
-  ContentMainLikedCountBox,
-  ContentMainLikedCountText,
-  ContentMainLikedCountTip,
 } from './style';
 import {
   ICommentListItemProps,
 } from '../BaseCommentItem';
-import {
-  serviceHandleGetCommentUserInfo,
-} from 'src/pages/details/Details.service';
+import BaseCommentItemTitleAvatar from './base_comment_item_title_avatar/BaseCommentItemTitleAvatar';
 
 
 export interface IBaseCommentItemTitleProps extends ICommentListItemProps { };
-interface IBaseCommentItemTitleState {
-  loading: boolean;
-  userInfo: {
-    _id: string,
-    username: string,
-    useravatar: string,
-    articlesCount: number,
-    followersCount: number,
-  };
-};
 
 
 const BaseCommentItemTitle = React.memo((
   props: IBaseCommentItemTitleProps,
 ): JSX.Element => {
-
-  const [
-    state, setState
-  ] = React.useState<IBaseCommentItemTitleState>({
-    loading: false,
-    userInfo: {
-      _id: '',
-      username: '',
-      useravatar: '',
-      articlesCount: 0,
-      followersCount: 0,
-    },
-  });
-
-  /**
-   * 初始化处理头像框 popover title
-   */
-  function handleInitAvatarPopoverTitle(): JSX.Element {
-    return (
-      <PopoverTitleContainer>
-        <Spin spinning={state.loading}>
-          <PopoverTitleMain>
-            <TitleMainAvatar>
-              <Avatar
-                // src={props.commentInfo.from.useravatar}
-                src={state.userInfo.useravatar}
-                icon="user"
-                shape="square"
-                alt="评论者"
-                style={{
-                  width: '4.375rem',
-                  height: '4.375rem',
-                  transform: 'translateY(-1.25rem)',
-                }}
-              />
-            </TitleMainAvatar>
-            <TitleMainName>{
-              // props.commentInfo.from.username
-              state.userInfo.username
-            }</TitleMainName>
-          </PopoverTitleMain>
-        </Spin>
-      </PopoverTitleContainer>
-    );
-  }
-
-  /**
-   * 初始化处理头像框 popover content
-   */
-  function handleInitAvatarPopoverContent(): JSX.Element {
-    return (
-      <PopoverContentContainer>
-        <Spin spinning={state.loading}>
-          <PopoverContentMain>
-            <Row>
-              <Col span={8}>
-                <ContentMainArticleCountBox>
-                  <ContentMainArticleCountTip>
-                    文章
-                </ContentMainArticleCountTip>
-                  <ContentMainArticleCountText>
-                    {state.userInfo.articlesCount}
-                  </ContentMainArticleCountText>
-                </ContentMainArticleCountBox>
-              </Col>
-              <Col span={8}>
-                <ContentMainLikedCountBox>
-                  <ContentMainLikedCountTip>
-                    获赞
-                </ContentMainLikedCountTip>
-                  <ContentMainLikedCountText>
-                    0
-                </ContentMainLikedCountText>
-                </ContentMainLikedCountBox>
-              </Col>
-              <Col span={8}>
-                <ContentMainFocusedCountBox>
-                  <ContentMainFocusedCountTip>
-                    关注者
-                </ContentMainFocusedCountTip>
-                  <ContentMainFocusedCountText>
-                    {state.userInfo.followersCount}
-                  </ContentMainFocusedCountText>
-                </ContentMainFocusedCountBox>
-              </Col>
-            </Row>
-            <Row>
-              <Col span={12}>
-                <Button
-                  icon={'user-add'}
-                  type="primary"
-                >关注他</Button>
-              </Col>
-              <Col span={12}>
-                <Button
-                  icon={'message'}
-                  type="ghost"
-                >发私信</Button>
-              </Col>
-            </Row>
-          </PopoverContentMain>
-        </Spin>
-      </PopoverContentContainer>
-    );
-  }
-
-  /**
-   * 处理评论项头像框hover
-   */
-  function handleCommentAvatarHover(
-    visible: boolean,
-  ): void {
-    const { isReply } = props;
-    const {
-      _id,
-    } = props.commentInfo;
-
-    if (visible) {
-      setState({ ...state, loading: true, });
-
-      serviceHandleGetCommentUserInfo({
-        isReply,
-        commentId: _id,
-      }, (data) => {
-        const {
-          userInfo,
-        } = data.info;
-
-        setState({ userInfo, loading: false });
-      });
-    }
-  }
-
   return (
     <TitleContainer>
-      <Popover
+      {/* <Popover
         mouseEnterDelay={.7}
         title={handleInitAvatarPopoverTitle()}
         content={handleInitAvatarPopoverContent()}
@@ -205,8 +36,10 @@ const BaseCommentItemTitle = React.memo((
         style={{
           color: '#999',
         }}
-      >{props.commentInfo.from.username}</span>
-
+      >{props.commentInfo.from.username}</span> */}
+      <BaseCommentItemTitleAvatar
+        {...props}
+      />
     </TitleContainer>
   );
 });
---
+++ src/components/widget/base_comment_item/base_comment_item_title/base_comment_item_title_avatar/style.tsx
@@ -0,0 +1,51 @@
+import styled from 'styled-components';
+
+
+export const AvatarWrapper = styled.div``;
+
+// 评论者信息popover框
+export const PopoverTitleContainer = styled.div``;
+export const PopoverTitleMain = styled.div`
+  display: flex;
+`;
+export const TitleMainAvatar = styled.div``;
+export const TitleMainName = styled.div`
+  line-height: 4rem;
+  margin-left: 0.625rem;
+`;
+export const PopoverContentContainer = styled.div``;
+export const PopoverContentMain = styled.div`
+  width: 15rem;
+  font-weight: bold;
+  text-align: center;
+`;
+export const ContentMainArticleCountBox = styled.div``;
+export const ContentMainArticleCountText = styled.p`
+  font-size: 1.125rem;
+`;
+export const ContentMainArticleCountTip = styled.p`
+  color: #999;
+`;
+export const ContentMainLikedCountBox = styled.div``;
+export const ContentMainLikedCountText = styled.p`
+  font-size: 1.125rem;
+`;
+export const ContentMainLikedCountTip = styled.p`
+  color: #999;
+`;
+export const ContentMainFocusedCountBox = styled.div``;
+export const ContentMainFocusedCountText = styled.p`
+  font-size: 1.125rem;
+`;
+export const ContentMainFocusedCountTip = styled.p`
+  color: #999;
+`;
+
+
+// 用户名框
+export const AvatarNameBox = styled.div`
+  display: inline-block;
+`;
+export const AvatarNameText = styled.span`
+  color: #999;
+`;
