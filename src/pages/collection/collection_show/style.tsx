9113af4f137e9d1146c9a1a60403bc9438fa2549
yang <1766083035@qq.com>
2019-01-10 14:56:31 +0800

    :fire: 代码格式调整

--- src/pages/collection/collection_show/style.tsx
+++ src/pages/collection/collection_show/style.tsx
@@ -7,19 +7,7 @@ export const ItemContentBox = styled.div`
   color: #999;
   font-size: 0.875rem;
 `;
-
-export const ContentTip = styled.div`
-
-`;
-
-export const ContentTitle = styled.h2`
-
-`;
-
-export const ContentTag = styled.div`
-  
-`;
-
-export const ItemExtraBox = styled.div`
-
-`;
+export const ContentTip = styled.div``;
+export const ContentTitle = styled.h2``;
+export const ContentTag = styled.div``;
+export const ItemExtraBox = styled.div``;
