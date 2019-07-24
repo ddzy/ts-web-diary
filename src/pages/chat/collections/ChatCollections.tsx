4321332df2225be1b3073e0be273917c2db465a4
yang <1766083035@qq.com>
2019-07-24 16:52:20 +0800

    :construction: complete `ChatCollections` component, includes basic layout and style

--- src/pages/chat/collections/ChatCollections.tsx
+++ src/pages/chat/collections/ChatCollections.tsx
@@ -1,22 +1,66 @@
 import * as React from 'react';
+import * as Loadable from 'react-loadable';
+import {
+  Route,
+  Redirect,
+  withRouter,
+  RouteComponentProps,
+} from 'react-router-dom';
+import {
+  Collapse,
+} from 'antd';
 
 import {
   CollectionsWrapper,
   CollectionsMain,
 } from './style';
 
+const LoadableChatCollectionsImages = Loadable({
+  loader: () => import('./images/ChatCollectionsImages'),
+  loading: () => null,
+});
+const LoadableChatCollectionsFiles = Loadable({
+  loader: () => import('./files/ChatCollectionsFiles'),
+  loading: () => null,
+});
+const LoadableChatCollectionsTexts = Loadable({
+  loader: () => import('./texts/ChatCollectionsTexts'),
+  loading: () => null,
+});
 
-export interface IChatCollectionsProps {
+
+export interface IChatCollectionsProps extends RouteComponentProps {
 
 };
 
+const ChatCollections = React.memo((props: IChatCollectionsProps) => {
+  /**
+   * 处理Collapse切换
+   * @param type collapse类型,对应当前路由
+   */
+  function handleTabChange(type: string) {
+    type && props.history.push(`/chat/collections/${type}`);
+  }
 
-export default function ChatCollections(props: IChatCollectionsProps) {
   return (
     <CollectionsWrapper>
       <CollectionsMain>
-        我的收藏视图
+        <Route exact path="/chat/collections" render={() => <Redirect to="/chat/collections/images" />} />
+
+        <Collapse accordion onChange={handleTabChange}>
+          <Collapse.Panel header="收藏的图片" key="images">
+            <Route path="/chat/collections/images" component={LoadableChatCollectionsImages} />
+          </Collapse.Panel>
+          <Collapse.Panel header="收藏的文件" key="files">
+            <Route path="/chat/collections/files" component={LoadableChatCollectionsFiles} />
+          </Collapse.Panel>
+          <Collapse.Panel header="收藏的文本" key="texts">
+            <Route path="/chat/collections/texts" component={LoadableChatCollectionsTexts} />
+          </Collapse.Panel>
+        </Collapse>
       </CollectionsMain>
     </CollectionsWrapper>
   );
-}
+});
+
+export default withRouter(ChatCollections);
---
+++ src/pages/chat/collections/texts/style.tsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+
+export const TextsWrapper = styled.div``;
+export const TextsMain = styled.div`
+
+`;
