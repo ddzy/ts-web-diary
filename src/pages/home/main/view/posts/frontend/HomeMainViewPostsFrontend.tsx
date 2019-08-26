669f668b9adf57d172b21d23f82415600813ad46
yang <1766083035@qq.com>
2019-08-26 17:42:57 +0800

    :sparkles: a new feature, display article list in Home component with pagination

--- src/pages/home/main/view/posts/frontend/HomeMainViewPostsFrontend.tsx
+++ src/pages/home/main/view/posts/frontend/HomeMainViewPostsFrontend.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-// import * as InfiniteScroll from 'react-infinite-scroller';
+import * as InfiniteScroll from 'react-infinite-scroller';
 import {
   withRouter,
   RouteComponentProps,
@@ -30,23 +30,25 @@ export interface IHomeMainViewPostsFrontendProps extends RouteComponentProps {
 
 };
 export interface IHomeMainViewPostsFrontendState {
-  // ? 加载文章时的loading状态
-  isLoading: boolean;
+  // ? 首次加载文章时的loading状态
+  isFirstLoading: boolean;
 
+  // ? 分页加载时的loading状态
+  isLoadMoreLoading: boolean;
+  // ? 是否还有更多文章
+  hasMoreArticle: boolean;
   // ? 文章列表
-  articleList: ICommonBaseArticleInfo[],
+  articleList: ICommonBaseArticleInfo[];
 };
 
 
 const HomeMainViewPostsFrontend = React.memo((props: IHomeMainViewPostsFrontendProps) => {
   const [state, setState] = React.useState<IHomeMainViewPostsFrontendState>({
-    isLoading: false,
+    isFirstLoading: false,
+    isLoadMoreLoading: false,
+    hasMoreArticle: true,
     articleList: [],
   });
-
-  React.useEffect(() => {
-    _getArticleList();
-  }, []);
 
   /**
    * [初始化] - 文章列表项的图标
@@ -72,11 +74,16 @@ const HomeMainViewPostsFrontend = React.memo((props: IHomeMainViewPostsFrontendP
 
   /**
    * [获取] - 文章列表(分页)
+   * @param page 当前页数
+   * @param isFirst 是否首次加载
    */
-  function _getArticleList() {
+  function _getArticleList(
+    page: number,
+  ) {
     setState({
       ...state,
-      isLoading: true,
+      isFirstLoading: page === 1,
+      isLoadMoreLoading: true,
     });
 
     const userId = localStorage.getItem('userid');
@@ -101,125 +108,150 @@ const HomeMainViewPostsFrontend = React.memo((props: IHomeMainViewPostsFrontendP
       data: {
         type,
         userId,
+        page,
         pageSize: PAGE_SIZE,
-        page: 1,
       },
     }).then((res) => {
       const { articleList } = res.data;
 
       setState({
         ...state,
-        isLoading: false,
-        articleList,
+        isFirstLoading: false,
+        isLoadMoreLoading: false,
+        hasMoreArticle: articleList.length !== 0,
+        articleList: state.articleList.concat(articleList),
       });
     });
+  }
+
+  function handleLoadMoreArticleList(page: number) {
+    _getArticleList(page);
   }
 
   return (
     <FrontendWrapper>
       <FrontendMain>
-        <Skeleton
-          className="am-view-posts-loadlist"
-          active={true}
-          loading={state.isLoading}
+        <InfiniteScroll
+          hasMore={state.hasMoreArticle && !state.isLoadMoreLoading}
+          // pageStart={1}
+          initialLoad={true}
+          loadMore={handleLoadMoreArticleList}
         >
-          {
-            state.articleList.length === 0
-              ? <Empty description="此分类下暂时没有文章..." />
-              : (
-                <List
-                  itemLayout="vertical"
-                  size="default"
-                  dataSource={state.articleList}
-                  footer={
-                    <div>
-                      <p style={{
-                        textAlign: 'center',
-                      }}>
-                        <b>滚动以加载更多文章↓↓↓</b>
-                      </p>
-                    </div>
-                  }
-                  renderItem={item => (
-                    <List.Item
-                      key={item.title}
-                      actions={[
-                        _initIcon({
-                          type: 'eye-o',
-                          text: '156',
-                          key: 'list-vertical-eye-o',
-                        }),
-                        _initIcon({
-                          type: 'like-o',
-                          text: '108',
-                          key: 'list-vertical-like-o',
-                        }),
-                        _initIcon({
-                          type: 'info-circle',
-                          text: 'duan',
-                          key: 'list-vertical-info-circle',
-                        }),
-                        _initIcon({
-                          type: 'clock-circle-o',
-                          text: String(formatTime(item.create_time)),
-                          key: 'list-vertical-clock-circle-o',
-                        }),
-                      ]}
-                      extra={
-                        item.cover_img
-                          ? (
-                            <img
-                              width={272}
-                              height={168}
-                              alt="cover_img"
-                              src={item.cover_img}
+          <Skeleton
+            className="am-view-posts-loadlist"
+            active={true}
+            avatar={true}
+            paragraph={{
+              rows: 4,
+            }}
+            loading={state.isFirstLoading}
+          >
+            {
+              state.articleList.length === 0
+                ? <Empty description="此分类下暂时没有文章..." />
+                : (
+                  <List
+                    itemLayout="vertical"
+                    size="default"
+                    dataSource={state.articleList}
+                    footer={
+                      <div>
+                        <p style={{
+                          textAlign: 'center',
+                        }}>
+                          <b>
+                            {
+                              state.hasMoreArticle
+                                ? '滚动以加载更多文章↓↓↓'
+                                : '没有更多文章了...'
+                            }
+                          </b>
+                        </p>
+                      </div>
+                    }
+                    renderItem={(item) => (
+                      <List.Item
+                        key={item.title}
+                        actions={[
+                          _initIcon({
+                            type: 'eye-o',
+                            text: '156',
+                            key: 'list-vertical-eye-o',
+                          }),
+                          _initIcon({
+                            type: 'like-o',
+                            text: '108',
+                            key: 'list-vertical-like-o',
+                          }),
+                          _initIcon({
+                            type: 'info-circle',
+                            text: item.author.username,
+                            key: 'list-vertical-info-circle',
+                          }),
+                          _initIcon({
+                            type: 'clock-circle-o',
+                            text: String(formatTime(item.create_time)),
+                            key: 'list-vertical-clock-circle-o',
+                          }),
+                        ]}
+                        extra={
+                          item.cover_img
+                            ? (
+                              <img
+                                width={272}
+                                height={168}
+                                alt="cover_img"
+                                src={item.cover_img}
+                              />
+                            )
+                            : (
+                              <div
+                                style={{
+                                  width: 272,
+                                  height: 168,
+                                  border: '1px solid #f6f6f6',
+                                }}
+                              />
+                            )
+                        }
+                      >
+                        <List.Item.Meta
+                          avatar={
+                            <Avatar
+                              icon="user"
+                              size="large"
+                              src={item.author.useravatar}
                             />
-                          )
-                          : (
-                            <div
+                          }
+                          title={
+                            <p
                               style={{
-                                width: 272,
-                                height: 168,
-                                border: '1px solid #f6f6f6',
+                                fontSize: '18px',
                               }}
-                            />
-                          )
-                      }
-                    >
-                      <List.Item.Meta
-                        avatar={
-                          <Avatar
-                            icon="user"
-                            size="large"
-                            src={item.author.useravatar}
-                          />
-                        }
-                        title={
-                          <p
-                            style={{
-                              fontSize: '18px',
-                            }}
-                          >
-                            <Link to={`/details/${item._id}`}>{item.title}</Link>
-                          </p>
-                        }
-                      />
-                      {item.description}
-                      <Link to={`/details/${item._id}`}>...阅读原文</Link>
-                    </List.Item>
-                  )}
-                >
-                  <Skeleton
-                    className="am-view-posts-loadlist"
-                    loading={state.isLoading}
-                    active={true}
+                            >
+                              <Link to={`/details/${item._id}`}>{item.title}</Link>
+                            </p>
+                          }
+                        />
+                        {item.description}
+                        <Link to={`/details/${item._id}`}>...阅读原文</Link>
+                      </List.Item>
+                    )}
                   >
-                    <div />
-                  </Skeleton>
-                </List>
-              )
-          }
-        </Skeleton>
+                    <Skeleton
+                      className="am-view-posts-loadlist"
+                      active={true}
+                      avatar={true}
+                      paragraph={{
+                        rows: 5,
+                      }}
+                      loading={state.isLoadMoreLoading}
+                    />
+                  </List>
+                )
+            }
+          </Skeleton>
+        </InfiniteScroll>
       </FrontendMain>
     </FrontendWrapper>
   );
