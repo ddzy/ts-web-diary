13784c7cfa7e926fb6dd667e336fd05d28518f1c
yang <1766083035@qq.com>
2019-09-21 15:30:13 +0800

    :construction: update `BasePinEdit` component, can search topic from local topic list

--- src/components/widget/base_pin_edit/action/topic/content/BasePinEditActionTopicContent.tsx
+++ src/components/widget/base_pin_edit/action/topic/content/BasePinEditActionTopicContent.tsx
@@ -27,8 +27,10 @@ export interface IBasePinEditActionTopicContentProps extends RouteComponentProps
   ) => void;
 };
 export interface IBasePinEditActionTopicContentState {
-  // ? 话题列表
+  // ? 源话题列表
   topicList: IBaseCommonTopicInfo[];
+  // ? 便于搜索, 克隆的话题列表
+  searchTopicList: IBaseCommonTopicInfo[];
   // ? 获取话题列表时的loading状态
   isShowTopicListLoading: boolean;
 };
@@ -38,6 +40,7 @@ const BasePinEditActionTopicContent = React.memo((props: IBasePinEditActionTopic
   const [state, setState] = React.useState<IBasePinEditActionTopicContentState>({
     topicList: [],
     isShowTopicListLoading: false,
+    searchTopicList: [],
   });
 
   React.useEffect(() => {
@@ -82,6 +85,7 @@ const BasePinEditActionTopicContent = React.memo((props: IBasePinEditActionTopic
         setState({
           ...state,
           topicList: resData.topicList,
+          searchTopicList: resData.topicList,
           isShowTopicListLoading: false,
         });
       } else {
@@ -95,16 +99,47 @@ const BasePinEditActionTopicContent = React.memo((props: IBasePinEditActionTopic
     });
   }
 
+  /**
+   * [处理] - 搜索话题
+   * @description 在本地搜索, 因为一次性获取了所有的话题
+   */
+  function handleTopicSearch(
+    keyword: string,
+  ) {
+    if (!keyword) {
+      const newTopicList = state.topicList.slice();
+
+      setState({
+        ...state,
+        searchTopicList: newTopicList,
+      });
+    } else {
+      const originTopicList = state.topicList.slice();
+      const regTopicName = new RegExp(keyword, 'gi');
+
+      const newTopicList = originTopicList.filter((v) => {
+        return regTopicName.test(v.name);
+      });
+
+      setState({
+        ...state,
+        searchTopicList: newTopicList,
+      });
+    }
+  }
+
   return (
     <ContentWrapper>
       <ContentMain>
         {/* 话题搜索框区块 */}
-        <PinMainEditActionTopicContentSearch />
+        <PinMainEditActionTopicContentSearch
+          onTopicSearch={handleTopicSearch}
+        />
 
         {/* 话题列表展示区块 */}
         <Spin spinning={state.isShowTopicListLoading}>
           <PinMainEditActionTopicContentDisplay
-            topicList={state.topicList}
+            topicList={state.searchTopicList}
             onTopicContentChange={props.onTopicContentChange}
           />
         </Spin>
