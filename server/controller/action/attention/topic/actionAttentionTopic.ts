0d0ca573cebf47dc25367641cb4574ea885f4815
yang <1766083035@qq.com>
2019-09-30 15:34:17 +0800

    :sparkles: a new feature, now can attention topic at Topic Single page

--- server/controller/action/attention/topic/actionAttentionTopic.ts
+++ server/controller/action/attention/topic/actionAttentionTopic.ts
@@ -1,7 +1,76 @@
 import * as Router from 'koa-router';
 
+import {
+  Topic,
+  User,
+} from '../../../../model/model';
+
 
 const actionAttentionTopicController = new Router();
 
+
+/**
+ * [处理] - 关注 or 取消关注话题
+ */
+actionAttentionTopicController.post('/', async (ctx) => {
+  interface IRequestParams {
+    userId: string;
+    topicId: string;
+    isAttention: boolean;
+  };
+
+  const {
+    userId,
+    topicId,
+    isAttention,
+  } = ctx.request.body as IRequestParams;
+
+  try {
+    // ? 如果是关注
+    if (isAttention) {
+      // * 更新用户关注的话题列表
+      await User.findByIdAndUpdate(userId, {
+        '$addToSet': {
+          'attention.topics': topicId,
+        },
+      });
+
+      // * 更新话题的关注者列表
+      await Topic.findByIdAndUpdate(topicId, {
+        '$addToSet': {
+          'followers': userId,
+        },
+      });
+    } else {
+      await User.findByIdAndUpdate(userId, {
+        '$pull': {
+          'attention.topics': topicId,
+        },
+      });
+
+      await Topic.findByIdAndUpdate(topicId, {
+        '$pull': {
+          'followers': userId,
+        },
+      });
+    }
+
+    ctx.body = {
+      code: 0,
+      message: isAttention ? '已成功关注该话题!' : '已取消关注该话题!',
+      data: {
+        attentionInfo: {
+          isAttention,
+        },
+      },
+    };
+  } catch (error) {
+    ctx.body = {
+      code: 0,
+      message: '后端发生错误, 请稍后重试!',
+      data: error,
+    };
+  }
+});
 
 export default actionAttentionTopicController;
--- src/pages/topic/single/aside/info/content/TopicSingleAsideInfoContent.tsx
+++ src/pages/topic/single/aside/info/content/TopicSingleAsideInfoContent.tsx
@@ -2,6 +2,8 @@ import * as React from 'react';
 import {
   Button,
   Avatar,
+  notification,
+  message,
 } from 'antd';
 
 import {
@@ -15,6 +17,7 @@ import {
   ContentMainDescription,
 } from './style';
 import { IStaticTopicInfo } from 'pages/topic/Topic.types';
+import { query } from 'services/request';
 
 
 export interface ITopicSingleAsideInfoContentProps {
@@ -23,10 +26,72 @@ export interface ITopicSingleAsideInfoContentProps {
     is_attention: boolean;
   };
 };
-export interface ITopicSingleAsideInfoContentState { };
+export interface ITopicSingleAsideInfoContentState {
+  // ? 用户是否关注了该话题
+  isAttentionTopic: boolean;
+};
 
 
 const TopicSingleAsideInfoContent = React.memo((props: ITopicSingleAsideInfoContentProps) => {
+  const [state, setState] = React.useState<ITopicSingleAsideInfoContentState>({
+    isAttentionTopic: false,
+  });
+
+  React.useEffect(() => {
+    setState({
+      ...state,
+      isAttentionTopic: props.topicInfo.is_attention,
+    });
+  }, [props.topicInfo]);
+
+
+  /**
+   * [处理] - 关注 or 取消关注话题
+   */
+  function handleAttentionBtnClick() {
+    /* 用户鉴权 */
+    const userId = localStorage.getItem('userid');
+
+    if (!userId) {
+      notification.error({
+        message: '错误',
+        description: '用户凭证已丢失, 请重新登录!',
+      });
+    }
+
+    const topicId = props.topicInfo._id;
+    const oldIsAttention = state.isAttentionTopic;
+    const newIsAttention = !oldIsAttention;
+
+    query({
+      jsonp: false,
+      method: 'POST',
+      url: '/api/action/attention/topic',
+      data: {
+        userId,
+        topicId,
+        isAttention: newIsAttention,
+      },
+    }).then((res) => {
+      const resCode = res.code;
+      const resMessage = res.message;
+      const resData = res.data;
+
+      if (resCode === 0) {
+        const attentionInfo = resData.attentionInfo;
+
+        setState({
+          ...state,
+          isAttentionTopic: attentionInfo.isAttention,
+        });
+
+        message.success(resMessage);
+      } else {
+        message.error(resMessage);
+      }
+    });
+  }
+
   return (
     <ContentWrapper>
       <ContentMain>
@@ -50,9 +115,10 @@ const TopicSingleAsideInfoContent = React.memo((props: ITopicSingleAsideInfoCont
         <ContentMainAttentionBox>
           <Button
             type="primary"
+            onClick={handleAttentionBtnClick}
           >
             {
-              props.topicInfo.is_attention ? '取消关注' : '关注'
+              state.isAttentionTopic ? '取消关注' : '关注'
             }
           </Button>
         </ContentMainAttentionBox>
